faff644012ed949e366cac519b63dd9ba976057d
jeffskinnerbox <jeff.irland@verizon.net>
2014-03-12 10:51:04 -0500

    Update documentation

---
+++ images/summary/create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Convert takes an input file, in some image format, and converts it to a different image format.
+# In addition, various types of image processing can be performed on the converted
+# image during the conversion process. Convert recognizes the image formats supported
+# by ImageMagick.
+# 
+#     See [ImageMagick](http://linux.about.com/library/cmd/blcmdl1_ImageMagick.htm)
+#         [convert](http://linux.about.com/od/commands/l/blcmdl1_convert.htm)
+#
+
+IMAGESRC="."
+IMAGEDES="thumbnail"
+IMAGESIZE="90x90"
+
+for file in $( ls $IMAGESRC/*.jpg $IMAGESRC/*.png )
+do
+    convert -size $IMAGESIZE $file -resize $IMAGESIZE +profile '*' $IMAGEDES/$file
+done
